0ff304f1ef811e6fc9b14ff98c1211c0c5d2c248
aarrechea <adrianarrechea@yahoo.com.ar>
2024-11-13 08:32:56 -0300

    user admin added

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,23 @@
+# Imports
 from django.contrib import admin
+from apps.user.models import User
 
-# Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 
+        'created', 
+        'updated', 
+        'email',
+        'first_name',
+        'last_name',
+        'eva_closed',
+        'eva_in_progress',
+        'user_type',
+        'photo',
+        'is_staff',
+        'is_active',
+        'is_superuser',
+    )
