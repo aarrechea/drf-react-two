151bdf2ddb7d0c1f0bf09ecabfe5e710c7e25e9f
aarrechea <adrianarrechea@yahoo.com.ar>
2024-11-15 11:05:51 -0300

    added if debug == true in settings

--- CompanyRest/settings.py
+++ CompanyRest/settings.py
@@ -104,27 +104,28 @@ WSGI_APPLICATION = 'CompanyRest.wsgi.application'
 
 
 # Databases -----------------
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "intracen_dos",
-        'USER': 'postgres',
-        'PASSWORD': 'postgres', 
-        'HOST': '127.0.0.1', 
-        'PORT': '5432', 
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': "intracen_dos",
+            'USER': 'postgres',
+            'PASSWORD': 'postgres', 
+            'HOST': '127.0.0.1', 
+            'PORT': '5432', 
+        }
     }
-} """
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DATABASE_NAME"), 
-        'USER': os.getenv("DATABASE_USER"),
-        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
-        'HOST': os.getenv("DATABASE_HOST"),
-        'PORT': os.getenv("DATABASE_PORT"), 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv("DATABASE_NAME"), 
+            'USER': os.getenv("DATABASE_USER"),
+            'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+            'HOST': os.getenv("DATABASE_HOST"),
+            'PORT': os.getenv("DATABASE_PORT"), 
+        }
     }
-}
 
 
 # Password validation
